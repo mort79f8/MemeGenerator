c280ad0160c5764660d8b27831b792d29d55078c
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-09 17:12:13 +0200

    Images are now inserted via a for loop

--- S2.AspNet.Repetition/Pages/Index.cshtml.cs
+++ S2.AspNet.Repetition/Pages/Index.cshtml.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
 
 namespace S2.AspNet.Repetition.Pages
 {
     public class IndexModel : PageModel
     {
-
+        public string InsertImagesUrl(int memeImage)
+        {
+            return $"/Img/meme{memeImage}.png";
+        }
     }
+
 }
