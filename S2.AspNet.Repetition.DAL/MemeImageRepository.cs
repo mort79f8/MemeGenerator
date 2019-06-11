5983f2c33c832ed7ec55cf801c4967f9bf952ac5
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-11 10:52:33 +0200

    Started on getting the data from a database

---
+++ S2.AspNet.Repetition.DAL/MemeImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.AspNet.Repetition.DAL
+{
+    class MemeImageRepository : RepositoryBase
+    {
+    }
+}
---
+++ S2.AspNet.Repetition.Entities/MemeImage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.AspNet.Repetition.Entities
+{
+    class MemeImage
+    {
+        private int id;
+        private string url;
+        private string altText;
+
+        public MemeImage()
+        {
+        }
+
+        public MemeImage(int id, string url, string altText)
+        {
+            Id = id;
+            Url = url;
+            AltText = altText;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Url { get => url; set => url = value; }
+        public string AltText { get => altText; set => altText = value; }
+
+        
+    }
+}
