32d2092f6eb8e60c65a29e11d1ba652bbf3ce8f1
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-11 13:12:45 +0200

    filled out properties for MemeCreation class

--- S2.AspNet.Repetition.Entities/MemeCreation.cs
+++ S2.AspNet.Repetition.Entities/MemeCreation.cs
@@ -6,5 +6,35 @@ namespace S2.AspNet.Repetition.Entities
 {
     class MemeCreation
     {
+        private int id;
+        private MemeImage memeImg;
+        private DateTime timeStamp;
+        private string memeText;
+        private string position;
+        private string color;
+        private string size;
+
+        public MemeCreation()
+        {
+        }
+
+        public MemeCreation(int id, MemeImage memeImg, DateTime timeStamp, string memeText, string position, string color, string size)
+        {
+            Id = id;
+            MemeText = memeText;
+            TimeStamp = timeStamp;
+            MemeText = memeText;
+            Position = position;
+            Color = color;
+            Size = size;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public MemeImage MemeImg { get => memeImg; set => memeImg = value; }
+        public DateTime TimeStamp { get => timeStamp; set => timeStamp = value; }
+        public string MemeText { get => memeText; set => memeText = value; }
+        public string Position { get => position; set => position = value; }
+        public string Color { get => color; set => color = value; }
+        public string Size { get => size; set => size = value; }
     }
 }
